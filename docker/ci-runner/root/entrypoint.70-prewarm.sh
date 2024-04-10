077c4593e248d6925c2f6cb5d89e8169b03a4e4b
Dimi Kot <dimi.kot.code@gmail.com>
2024-04-09 19:43:43 -0700

    Do not prewarm tmpfs, just print df stats instead

--- docker/ci-runner/root/entrypoint.70-prewarm.sh
+++ docker/ci-runner/root/entrypoint.70-prewarm.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 #
-# Keeps /mnt directory in cache to lower the chances of the directory entries to
-# be evicted, and also, prints usage statistics.
+# Prints usage statistics and also, if /mnt is not on tmpfs, keeps the directory
+# in cache to lower the chances of the directory entries to be evicted.
 #
 set -u -e
 
 prewarm_loop() {
   sleep 30
   while :; do
-    tmp=/tmp/time_took
+    time_took=/tmp/time_took
+    mnt=/mnt
     export TIMEFORMAT="%R sec"
-    du=$({ time du -sh /mnt | sed -E 's/\s+/ /g'; } 2>$tmp)
-    echo "$(nice_date): Prewarm (took $(cat $tmp)): $du: $(uptime | sed -E -e 's/^\s*[0-9:]+\s+//' -e 's/\s+/ /g')"
+    info=$({ time df -h --output=fstype,target,used $mnt | tail -n1 | sed -E 's/[[:space:]]+/ /g'; } 2>$time_took)
+    if [[ "$info" != *tmpfs* ]]; then
+      info=$({ time du -sh $mnt | sed -E 's/\s+/ /g'; } 2>$time_took)
+    fi
+    uptime=$(uptime | sed -E -e 's/^\s*[0-9:]+\s+//' -e 's/\s+/ /g')
+    echo "$(nice_date): Prewarm (took $(cat $time_took)): $info: $uptime"
     sleep 60
   done
 }
 
-prewarm_loop &
+# Print "prewarm" stats only when TZ is set. This prevents it from printing in
+# debug dev environment of the client for instance.
+if [[ "$TZ" != "" ]]; then
+  prewarm_loop &
+fi
--- docker/ci-storage/entrypoint.70-prewarm.sh
+++ docker/ci-storage/entrypoint.70-prewarm.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #
-# Keeps /mnt directory in cache to lower the chances of the directory entries to
-# be evicted, and also, prints usage statistics.
+# Prints usage statistics and also, if /mnt is not on tmpfs, keeps the directory
+# in cache to lower the chances of the directory entries to be evicted.
 #
 set -u -e
 
 prewarm_loop() {
   sleep 30
   while :; do
-    tmp=/tmp/time_took
+    time_took=/tmp/time_took
+    mnt=/mnt
     export TIMEFORMAT="%R sec"
-    du=$({ time du -sh /mnt | sed -E 's/\s+/ /g'; } 2>$tmp)
-    echo "$(nice_date): Prewarm (took $(cat $tmp)): $du: $(uptime | sed -E -e 's/^\s*[0-9:]+\s+//' -e 's/\s+/ /g')"
+    info=$({ time df -h --output=fstype,target,used $mnt | tail -n1 | sed -E 's/[[:space:]]+/ /g'; } 2>$time_took)
+    if [[ "$info" != *tmpfs* ]]; then
+      info=$({ time du -sh $mnt | sed -E 's/\s+/ /g'; } 2>$time_took)
+    fi
+    uptime=$(uptime | sed -E -e 's/^\s*[0-9:]+\s+//' -e 's/\s+/ /g')
+    echo "$(nice_date): Prewarm (took $(cat $time_took)): $info: $uptime"
     sleep 60
   done
 }
