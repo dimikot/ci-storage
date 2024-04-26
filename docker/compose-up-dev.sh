b0ef8cf3c239b5a8bae0a38a5c01eaa03826dfb8
Dimi Kot <dimi.kot.code@gmail.com>
2024-04-25 18:53:12 -0700

    Simplify compose-up-dev.sh

--- docker/compose-up-dev.sh
+++ docker/compose-up-dev.sh
@@ -3,11 +3,9 @@ set -e
 
 echo "Building & booting containters on the local laptop for debugging purposes..."
 
-docker compose pull
-
 GH_TOKEN=$(gh auth token) \
 GH_REPOSITORY=$(gh repo view --json owner,name -q '.owner.login + "/" + .name') \
 GH_LABELS=ci-storage-dev \
 TZ=America/Los_Angeles \
 FORWARD_HOST=host.docker.internal \
-docker compose up --build "$@"
+docker compose up --pull=always --build "$@"
