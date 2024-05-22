683d90583b981c2c1759c3f05d2b434f0f80d8ca
Dimi Kot <dimi.kot.code@gmail.com>
2024-05-21 18:13:15 -0700

    Improve ActiveRunnersPercent counter; add jitter for metrics; add gh api diagnostics

--- docker/ci-runner/guest/entrypoint.20-cloudwatch.sh
+++ docker/ci-runner/guest/entrypoint.20-cloudwatch.sh
@@ -6,42 +6,47 @@ set -u -e
 
 cloudwatch_loop() {
   NAMESPACE="ci-storage/metrics"
-  INTERVAL_SEC=10
+  INTERVAL_SEC=30
   REGION=$(aws_metadata_curl latest/meta-data/placement/availability-zone | sed "s/[a-z]$//")
 
   while :; do
-    sleep "$INTERVAL_SEC"
+    duration=$(("$INTERVAL_SEC" + ("$RANDOM" * "$INTERVAL_SEC" / 4 / 32768)))
+    sleep "$duration"
 
-    runners=$(
-      gh api \
-        "repos/$GH_REPOSITORY/actions/runners" \
-        -q '.runners[] | {id,name,status,busy} | select(.name | startswith("ci-storage-"))' \
-        --paginate || true
-    )
-
-    if [[ "$runners" == "" ]]; then
+    res=$(gh api "repos/$GH_REPOSITORY/actions/runners" --paginate || true)
+    if ! grep -qF '"runners":' <<<"$res"; then
+      echo "$(nice_date): gh api returned result with no \"runners\" key:"
+      echo "$res"
       continue
     fi
 
+    runners=$(echo "$res" | jq '.runners[] | {id,name,status,busy} | select(.name | startswith("ci-storage-"))')
+
     idle_runner_ids=$(
-        echo "$runners" | jq -r 'select((.name | startswith("ci-storage-")) and (.busy == false) and (.status == "online")) | {id} | .[]'
+        echo "$runners" | jq -r 'select((.busy == false) and (.status == "online")) | {id} | .[]'
     )
     active_runner_ids=$(
-        echo "$runners" | jq -r 'select((.name | startswith("ci-storage-")) and (.busy == true) and (.status == "online")) | {id} | .[]'
+        echo "$runners" | jq -r 'select((.busy == true) and (.status == "online")) | {id} | .[]'
+    )
+    offline_runner_ids=$(
+        echo "$runners" | jq -r 'select((.status == "offline")) | {id} | .[]'
     )
     all_runner_ids=$(
-        echo "$runners" | jq -r '{id} | .[]'
+        echo "$runners" | jq -r 'select(true) | {id} | .[]'
     )
 
     # shellcheck disable=SC2034
-    IdleRunnersCount=$(echo "$idle_runner_ids" | wc -w)
-    ActiveRunnersCount=$(echo "$active_runner_ids" | wc -w)
-    AllRunnersCount=$(echo "$all_runner_ids" | wc -w)
+    IdleRunnersCount=$(($(echo "$idle_runner_ids" | wc -w)))
+    ActiveRunnersCount=$(($(echo "$active_runner_ids" | wc -w)))
     # shellcheck disable=SC2034
-    ActiveRunnersPercent=$(("$AllRunnersCount" == 0 ? 100 : 100 * "$ActiveRunnersCount" / "$AllRunnersCount"))
+    OfflineRunnersCount=$(($(echo "$offline_runner_ids" | wc -w)))
+    # shellcheck disable=SC2034
+    AllRunnersCount=$(($(echo "$all_runner_ids" | wc -w)))
+    # shellcheck disable=SC2034
+    ActiveRunnersPercent=$((("$IdleRunnersCount" + "$ActiveRunnersCount") == 0 ? 100 : 100 * "$ActiveRunnersCount" / ("$IdleRunnersCount" + "$ActiveRunnersCount")))
 
     out=()
-    for metric in IdleRunnersCount ActiveRunnersCount AllRunnersCount ActiveRunnersPercent; do
+    for metric in IdleRunnersCount ActiveRunnersCount OfflineRunnersCount AllRunnersCount ActiveRunnersPercent; do
       out+=("$metric=${!metric}")
       if [[ "$REGION" != "" ]]; then
         suffix="publishing to CloudWatch"
