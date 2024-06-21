33667d000414c5df39fa71365a439632643d9c36
Dimi Kot <dimi.kot.code@gmail.com>
2024-06-21 01:49:42 -0700

    Improve dind_loop debugging

--- docker/ci-runner/root/entrypoint.50-ssh-keys.sh
+++ docker/ci-runner/root/entrypoint.50-ssh-keys.sh
@@ -7,7 +7,7 @@ set -u -e
 secret_file=/run/secrets/CI_STORAGE_PRIVATE_KEY
 if [[ -f "$secret_file" ]]; then
   cat "$secret_file" > ~guest/.ssh/id_rsa
-  chmod 600 ~guest/.ssh/*
 fi
 
-chown -R guest:guest ~guest/.ssh
+chmod 600 ~guest/.ssh/* || true
+chown -R guest:guest ~guest/.ssh || true
--- docker/ci-runner/root/entrypoint.60-dind.sh
+++ docker/ci-runner/root/entrypoint.60-dind.sh
@@ -7,11 +7,13 @@ set -u -e
 
 # We use the init script and not systemd, because containerd's systemctl script
 # tries to modprobe, fails and shows a nasty warning.
+rm -f /var/run/docker.pid || true
 /etc/init.d/docker start
 echo
 
 dind_loop() {
   for _n in {0..9}; do
+    sleep 2
     if ! pgrep dockerd >/dev/null; then
       log=/var/log/docker.log
       echo
@@ -19,11 +21,10 @@ dind_loop() {
       echo "- Maybe sysbox is not installed on the host system?"
       echo "- Or maybe you run this container on a Docker Desktop host?"
       echo "Logs from $log:"
-      tail -n 5 $log | sed -E "s/^ +//" | grep . || true
+      tail -n 15 $log | sed -E "s/^ +//" | grep . || true
       echo
       break
     fi
-    sleep 1
   done
 }
 
