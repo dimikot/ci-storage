421ea57488cf7242805b4ec09e8e5e4f8e798761
Dimi Kot <dimi.kot.code@gmail.com>
2024-10-06 23:46:27 -0700

    Absolutize local_dir in the action for better slug(local-dir) purposes
    Pull Request: https://github.com/dimikot/ci-storage/pull/25 (main)

--- docker/ci-runner/guest/entrypoint.01-ci-storage-load.sh
+++ docker/ci-runner/guest/entrypoint.01-ci-storage-load.sh
@@ -6,6 +6,8 @@ set -u -e
 
 echo "$CI_STORAGE_HOST" > ci-storage-host
 
+# For some reason, the default GitHub Runner's work directory is
+# "$WORK_DIR/{repo-name}/{repo-name}" (i.e. repo-name is repeated twice).
 local_dir=$WORK_DIR/${GH_REPOSITORY##*/}/${GH_REPOSITORY##*/}
 
 mkdir -p "$local_dir"
@@ -18,7 +20,7 @@ if [[ "$CI_STORAGE_HOST" != "" && -f ~/.ssh/id_rsa ]]; then
   say "Running the initial \"ci-storage load\" for $local_dir..."
   ci-storage load \
     --storage-host="$CI_STORAGE_HOST" \
-    --storage-dir="$WORK_DIR/$GH_REPOSITORY/_" \
+    --storage-dir="$WORK_DIR/$GH_REPOSITORY/$(realpath "$local_dir" | tr / _)" \
     --slot-id="*" \
     --local-dir="$local_dir" \
     & export CI_STORAGE_PID=$!
