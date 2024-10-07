d42c4e879ce2518f7e7813104d0792d8bde1a884
Dimi Kot <dimi.kot.code@gmail.com>
2024-10-07 00:00:22 -0700

    Absolutize local_dir in the action for better slug(local-dir) purposes (#25)
    - ➡ #25
    
    (The stack is managed by
    [git-grok](https://github.com/dimikot/git-grok).)

--- docker/ci-runner/root/entrypoint.50-ssh-keys.sh
+++ docker/ci-runner/root/entrypoint.50-ssh-keys.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #
-# Puts SSH keys into the guest's home directory.
+# Puts SSH keys into the guest's and root's home directories.
 #
 set -u -e
+
+mkdir -p ~guest/.ssh && chmod 700 ~guest/.ssh
+mkdir -p ~root/.ssh && chmod 700 ~root/.ssh
 
 secret_file=/run/secrets/CI_STORAGE_PRIVATE_KEY
 if [[ -f "$secret_file" ]]; then
   cat "$secret_file" > ~guest/.ssh/id_rsa
+  cat "$secret_file" > ~root/.ssh/id_rsa
 fi
 
 chmod 600 ~guest/.ssh/* || true
 chown -R guest:guest ~guest/.ssh || true
+
+chmod 600 ~root/.ssh/* || true
+chown -R root:root ~root/.ssh || true
