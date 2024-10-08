021dd73cdb32baf83c60cc7a8ab132120de1d12d
Dimi Kot <dimi.kot.code@gmail.com>
2024-10-08 00:50:36 -0700

    Make TZ env var optional for ci-storage container and add validation

--- docker/ci-storage/root/entrypoint.01-validate.sh
+++ docker/ci-storage/root/entrypoint.01-validate.sh
@@ -4,6 +4,12 @@
 #
 set -u -e
 
+export TZ
+if [[ "${TZ:=}" != "" && ! "$TZ" =~ ^[-+_/a-zA-Z0-9]+$ ]]; then
+  say "If TZ is passed, it must be a valid TZ Idenfitier from https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+  exit 1
+fi
+
 secret_file=/run/secrets/CI_STORAGE_PUBLIC_KEY
 if [[ ! -f $secret_file ]]; then
   say "To access this container over SSH, a secret $(basename "$secret_file") or a mounted file $secret_file should exist. The container will start, but it's not accessible, which may be fine in dev environment."
