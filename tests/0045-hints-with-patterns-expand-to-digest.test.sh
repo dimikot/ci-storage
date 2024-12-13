e8385b7ccc4e774dbd60937c15aa6b7ab814acc6
Dimi Kot <dimi.kot.code@gmail.com>
2024-12-12 21:39:19 -0800

    Add hint input to action.yml (#33)
    ## PRs in the Stack
    - ➡ #33
    
    (The stack is managed by
    [git-grok](https://github.com/dimikot/git-grok).)

--- tests/0045-hints-with-patterns-expand-to-digest.test.sh
+++ tests/0045-hints-with-patterns-expand-to-digest.test.sh
@@ -4,6 +4,7 @@ source ./common.sh
 echo "a" > "$LOCAL_DIR/file-new-a"
 echo "b" > "$LOCAL_DIR/file-new-b"
 echo "c" > "$LOCAL_DIR/file-c"
+
 ci-storage \
   --slot-id=myslot1 \
   --hint="aaa bbb" \
@@ -12,3 +13,14 @@ ci-storage \
   store
 
 grep -qF "hints=aaa bbb @f3bba3f02fb45a2a ccc" "$STORAGE_DIR/myslot1/.ci-storage.meta"
+
+ci-storage \
+  --slot-id=myslot2 \
+  --hint="
+    aaa bbb
+    @$LOCAL_DIR/file-new-* $LOCAL_DIR/file-c $LOCAL_DIR/file-absent?
+    ccc
+  " \
+  store
+
+grep -qF "hints=aaa bbb @f3bba3f02fb45a2a ccc" "$STORAGE_DIR/myslot2/.ci-storage.meta"
